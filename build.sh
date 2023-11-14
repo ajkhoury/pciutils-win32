2666a93b9432b2a38cc3f4f755f5c73b7c6c7420
Aidan Khoury <aidan@revers.engineering>
2023-11-13 20:06:39 -0400

    Update build script for mingw32-make tool

--- build.sh
+++ build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-make clean
-make \
+MAKE_TOOL=make
+if ! [ -x "$(command -v make)" ]; then
+    MAKE_TOOL=mingw32-make
+else
+    MAKE_TOOL=make
+fi
+
+$MAKE_TOOL clean
+$MAKE_TOOL \
     CC=x86_64-w64-mingw32-gcc \
     HOST=x86_64-windows \
-    CFLAGS='-Og -g -Wall -W -Wno-format -Wno-cast-function-type -Wno-unused-function -Wno-parentheses -Wstrict-prototypes -Wmissing-prototypes' \
+    CFLAGS='-O2 -Wall -W -Wno-format -Wno-cast-function-type -Wno-unused-function -Wno-parentheses -Wstrict-prototypes -Wmissing-prototypes' \
     LDFLAGS='-static' \
     ZLIB=yes \
     IDSDIR="" \
     COMPAT_GETOPT=yes \
-    -j11
+    $@
